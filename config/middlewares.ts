ce6b75f037248c2ac1f5aff1d1593b240a5970a4
Akhmad Ridlo <akhmadd432@gmail.com>
2025-09-30 14:04:05 +0700

    ganti CORS GAti CORS

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,22 +1,20 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
   {
     name: 'strapi::cors',
     config: {
       origin: [
-        'http://localhost:1337',
-        'http://localhost:3000',
-        '[https://astacipta-dashboard.vercel.app](https://astacipta-dashboard.vercel.app)', // <-- PASTIKAN INI BENAR
+        'https://astacipta-dashboard.vercel.app', // <-- PASTIKAN INI BENAR
       ],
       headers: '*',
     },
   },
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+'strapi::security',
+'strapi::poweredBy',
+'strapi::logger',
+'strapi::query',
+'strapi::body',
+'strapi::session',
+'strapi::favicon',
+'strapi::public',
 ];
