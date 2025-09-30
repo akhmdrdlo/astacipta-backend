174a080300ac5f3a89f4ce37173186384db3a787
Akhmad Ridlo <akhmadd432@gmail.com>
2025-09-30 10:26:30 +0700

    feat. fixing backend bugs

--- config/server.ts
+++ config/server.ts
@@ -1,11 +1,8 @@
 // path: astacipta-backend/config/server.js
-
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    // Membaca kunci dari environment variable bernama APP_KEYS
-    // dan membaginya menjadi array berdasarkan koma
     keys: env('APP_KEYS').split(','),
   },
 });
