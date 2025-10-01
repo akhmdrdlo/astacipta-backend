5eb443128298d98b7971c5a84405343a8c0c49c3
Akhmad Ridlo Rifa'i <akhmadd432@gmail.com>
2025-10-01 13:14:54 +0700

    Update plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -13,6 +13,10 @@ module.exports = ({ env }) => ({
       providerOptions: {
         accessKeyId: env('CLOUDFLARE_ACCESS_KEY_ID'),
         secretAccessKey: env('CLOUDFLARE_SECRET_ACCESS_KEY'),
+        // ==== PERBAIKAN DI SINI ====
+        // Menambahkan 'region' yang dibutuhkan oleh AWS SDK
+        region: env('CLOUDFLARE_R2_REGION', 'auto'), 
+        // ============================
         endpoint: `https://${env('CLOUDFLARE_ACCOUNT_ID')}.r2.cloudflarestorage.com`,
         params: {
           Bucket: env('CLOUDFLARE_R2_BUCKET_NAME'),
